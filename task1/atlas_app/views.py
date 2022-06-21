650d5d35757957e255b156a64cd7840912671877
yakul-crossml <88380853+yakul-crossml@users.noreply.github.com>
2022-06-21 10:39:14 +0530

    Delete task1 directory

--- task1/atlas_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'form.html')
-
-
-def datatables(request):
-    return render(request, 'datatables.html')
